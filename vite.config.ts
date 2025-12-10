6cde21e39fa979bc7a3fc1f161ec49704ae0c34c
Miarotiana Stephan <miarotianastephan01@gmail.com>
2025-12-10 10:41:16 +0300

    update node module

--- vite.config.ts
+++ vite.config.ts
@@ -5,16 +5,24 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    host:true,
+  server: {
+    host: true,
   },
+
+  // Important pour Ionic + Vercel
+  base: './',
+
+  build: {
+    outDir: 'dist',
+  },
+
   plugins: [
     react(),
     legacy(),
-    tailwindcss()
+    tailwindcss(),
   ],
+
   test: {
     globals: true,
     environment: 'jsdom',
